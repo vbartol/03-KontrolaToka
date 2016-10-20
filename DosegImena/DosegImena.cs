f2c32b68d76665c41224f42c43b0842c6825e1df
VsiTeStudent <VsiTeStudent@STUDENT>
2016-10-20 19:50:53 +0200

    Prvi test gotov

--- DosegImena/DosegImena.cs
+++ DosegImena/DosegImena.cs
@@ -17,13 +17,13 @@ namespace Vsite.CSharp
         {
             string a = "Lokalna varijabla";
 
-            // TODO: Ispisati vrijednosti lokalne varijable a
-
-            // TODO: Ispisati vrijednost člana klase a
-
+            
+            Console.WriteLine(a);
+            
+            Console.WriteLine(this.a);
             {
-                // TODO: Definirati novu varijablu a unutar ovog bloka i ispisati njenu vrijednost
-
+                
+               // nedozvoljeno: int a=5
             }
         }
 
