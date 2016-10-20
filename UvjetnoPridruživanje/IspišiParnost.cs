ef05353aadb2c5b88ff9904b84b1185218abc290
VsiTeStudent <VsiTeStudent@STUDENT>
2016-10-20 20:25:10 +0200

    tu je mozda 1 krivi jer treb aima 9 testova,  a ima 8

--- UvjetnoPridruživanje/IspišiParnost.cs
+++ UvjetnoPridruživanje/IspišiParnost.cs
@@ -8,7 +8,7 @@ namespace Vsite.CSharp
         {
             // TODO: Napisati uvjetno pridruživanje ?: koje će ovisno o dijeljivosti 
             // broja varijabli odgovor dodijeliti vrijednost "paran" ili "neparan"
-            string odgovor = "neparan";
+            string odgovor = broj % 2 == 0 ? "paran" : "neparan";
 
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
         }
@@ -18,6 +18,10 @@ namespace Vsite.CSharp
             // TODO: Korištenjem grananja if dodijeliti varijabli odgovor vrijednost 
             // "paran" ili "neparan" ovisno o djeljivosti zadanog broja
             string odgovor = "neparan";
+            if (broj % 2 == 0)
+            
+                odgovor = " paran";
+            
 
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
         }
